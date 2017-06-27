8fe50982c161a5fbd95f38661461279a3b6cc3f7
YF <yangfeng@wepiao.com>
2017-06-27 14:54:47 +0800

    Initial Commit

---
+++ YFDefaultProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFDefaultProject
+//
+//  Created by YF on 2017/6/27.
+//  Copyright © 2017年 YF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFDefaultProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFDefaultProject
+//
+//  Created by YF on 2017/6/27.
+//  Copyright © 2017年 YF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
